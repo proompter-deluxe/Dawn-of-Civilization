9eb5b1dfb73dd087a0698dd9b245fccf50916977
crossphazer <crossphazer@hotmail.com>
2024-09-12 15:41:04 -0400

    Bring Arabian decline sooner
    900 AD is still too aggressive for entering crisis, so I set it to 1000 AD

--- Assets/Python/Consts.py
+++ Assets/Python/Consts.py
@@ -509,7 +509,7 @@ iKhmer : 1200,
 iMali : 1600,
 iByzantium : 1400,
 iTurks : 1507,
-iArabia : 1200,
+iArabia : 1000,
 iTibet : 1500,
 iMoors : 1150,
 iJava: 1645, # the decline of Sultanate of Mataram started then, even if the end was in 1755
