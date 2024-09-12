e9af2362269705cf025eb6f106e16470b8e9a06f
crossphazer <crossphazer@hotmail.com>
2024-09-12 18:17:10 -0400

    Persia between 870 and 1000 respawns quicker.

--- Assets/Python/RFCUtils.py
+++ Assets/Python/RFCUtils.py
@@ -726,8 +726,10 @@ def canRespawn(iCiv):
 	if player(iCiv).isAlive():
 		return False
 		
-	# check if only recently died
-	if data.civs[iCiv].iLastTurnAlive > turn() - turns(20):
+	# check if only recently died, excepting Persia as an emirate respawn
+	if iCiv == iPersia and year().between(870, 1000) and data.civs[iCiv].iLastTurnAlive > turn() - turns(5):
+		return False
+	elif data.civs[iCiv].iLastTurnAlive > turn() - turns(20):
 		return False
 	
 	# check if the civ can be reborn at this date
