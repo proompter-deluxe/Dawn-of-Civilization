e5207aba9e1b0ede64b99fbf1e974063fb9f9dc3
crossphazer <crossphazer@hotmail.com>
2024-09-16 02:15:22 -0400

    Do not give AI China Alloys
    China will have researched it by the time it has the workers to cut trees.

--- Assets/Python/Rise.py
+++ Assets/Python/Rise.py
@@ -505,8 +505,7 @@ class Birth(object):
 				plot.resetBirthProtected()
 				
 	def assignAdditionalTechs(self):
-		if self.iCiv == iChina and scenario() == i3000BC and not self.isHuman():
-			self.team.setHasTech(iAlloys, True, self.iPlayer, False, False)
+		return
 	
 	def assignAttributes(self):
 		# civilization attributes
