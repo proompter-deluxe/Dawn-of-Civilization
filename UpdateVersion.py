e6109c8e48f166cc95146d5fba1de0d25cc2aa14
crossphazer <crossphazer@hotmail.com>
2024-10-05 19:13:26 -0400

    Change the version to 2.0.0

--- UpdateVersion.py
+++ UpdateVersion.py
@@ -1,27 +1,32 @@
 import re
+import sys
 
 
 DEFINES_FILE_PATH = "Assets/XML/GlobalDefinesVersion.xml"
 
+if __name__ == "__main__":
+	args = sys.argv[1:]
 
-def update_version():
 	with open(DEFINES_FILE_PATH, "r+") as file:
 		file_content = file.read()
 		version = re.search(r"<DefineTextVal>(.*)</DefineTextVal>", file_content).group(1)
 		
-		if "-" in version:
-			base_version, suffix = version.split("-")
-			new_version = f"{base_version}-{int(suffix)+1}"
+		if "." in version:
+			major, minor, subminor = version.split(".")
+
+			new_version = version
+
+			if len(args) > 0 and args[0] == 'major':
+				new_version = f"{int(major)+1}.{minor}.{subminor}"
+			elif len(args) > 0 and args[0] == 'minor':
+				new_version = f"{major}.{int(minor)+1}.{subminor}"
+			else:
+				new_version = f"{major}.{minor}.{int(subminor)+1}"
+
+			print(f"Updated mod version to {new_version}")
+		
+			file.seek(0)
+			file.write(file_content.replace(version, new_version))
+			file.truncate()
 		else:
-			print("Found major mod version, did not increment")
-			return
-		
-		print(f"Update mod version to {new_version}")
-		
-		file.seek(0)
-		file.write(file_content.replace(version, new_version))
-		file.truncate()
-
-
-if __name__ == "__main__":
-	update_version()
+			print("Unexpected format, did not increment")
