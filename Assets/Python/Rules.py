534c8c052d7846e77e878aec951a6e2f8682d422
crossphazer <crossphazer@hotmail.com>
2024-12-30 01:54:22 -0500

    Modify how enslavement chance is calculated
    Don't stack odds, return from the most probable case.

--- Assets/Python/Rules.py
+++ Assets/Python/Rules.py
@@ -148,19 +148,19 @@ def captureSlaves(winningUnit, losingUnit):
 	if civ(losingUnit) == iNative:
 		if player(winningUnit).isSlavery() or player(winningUnit).isColonialSlavery():
 			captureUnit(losingUnit, winningUnit, iSlave, 50)
-			return
+		return
 	
 	# Nigeria UP: can always capture slaves from any civ at 25% rate (natives still at 50%)
 	if civ(winningUnit) == iNigeria:
 		if player(winningUnit).isSlavery() or player(winningUnit).isColonialSlavery():
 			captureUnit(losingUnit, winningUnit, iSlave, 25)
-			return
+		return
 
 	# also enslave barbarians but at a lesser rate
 	if civ(losingUnit) == iBarbarian:
 		if player(winningUnit).isSlavery() or player(winningUnit).isColonialSlavery():
 			captureUnit(losingUnit, winningUnit, iSlave, 15)
-			return
+		return
 
 @handler("combatResult")
 def mayanHolkanAbility(winningUnit, losingUnit):
